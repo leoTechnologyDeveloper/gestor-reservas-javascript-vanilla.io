f4fc13d7a288824762eaac2bc511050f7fef55be
leoTechnologyDeveloper <www.leotechnology@gmail.com>
2023-04-21 16:13:43 -0500

    listado reservas with Buttons

--- js/reservas.js
+++ js/reservas.js
@@ -6,6 +6,8 @@ const inputTelefono = document.querySelector('#telefono');
 const inputFecha = document.querySelector('#fecha');
 const inputHora = document.querySelector('#hora');
 const inputCantidad = document.querySelector('#cantidad');
+const reservasActualesContainer = document.querySelector('#reservasActualesContainer');
+
 
 // EVENT LISTENERS ********************************
 
@@ -59,69 +61,80 @@ class UI{
         document.querySelector('#main').insertBefore( divMensaje, document.querySelector('.contenido'));
 
         // Quitar el alert despues de 3 segundos
-        setTimeout( () => {
+        setTimeout( () => { 
             divMensaje.remove();
         }, 3000);
    }
 
-
-//    imprimirCitas({citas}) { // Se puede aplicar destructuring desde la función...
+// Se puede aplicar destructuring desde la función...
+   imprimirCitas({citas}) { 
        
-//     this.limpiarHTML();
+    this.limpiarHTML();
 
-//     citas.forEach(cita => {
-//         const {nombre, telefono, fecha, hora, cantidad, id } = cita;
+    citas.forEach(cita => {
+        const {nombre, telefono, fecha, hora, cantidad, id } = cita;
 
-//         const divCita = document.createElement('div');
-//         divCita.classList.add('bg-white', 'p-3');
-//         divCita.dataset.id = id;
+        const divCita = document.createElement('div');
+        divCita.classList.add('bg-gray-800', 'p-3', 'my-2');
+        divCita.dataset.id = id;
 
-//         // scRIPTING DE LOS ELEMENTOS...
-//         const mascotaParrafo = document.createElement('h2');
-//         mascotaParrafo.classList.add('bg-yellow', 'text-gray-800');
-//         mascotaParrafo.innerHTML = `${nombre}`;
+        // Scripting de los elementos...
+        const nombreH2 = document.createElement('h2');
+        nombreH2.classList.add('bg-yellow-300', 'text-gray-900');
+        nombreH2.innerHTML = `${nombre}`;
 
-//         const propietarioParrafo = document.createElement('p');
-//         propietarioParrafo.innerHTML = `<span class="font-weight-bolder">Propietario: </span> ${propietario}`;
+        const telefonoParrafo = document.createElement('p');
+        telefonoParrafo.innerHTML = `<span >Teléfono: </span> ${telefono}`;
 
-//         const telefonoParrafo = document.createElement('p');
-//         telefonoParrafo.innerHTML = `<span class="font-weight-bolder">Teléfono: </span> ${telefono}`;
+        const fechaParrafo = document.createElement('p');
+        fechaParrafo.innerHTML = `<span >Fecha: </span> ${fecha}`;
 
-//         const fechaParrafo = document.createElement('p');
-//         fechaParrafo.innerHTML = `<span class="font-weight-bolder">Fecha: </span> ${fecha}`;
+        const horaParrafo = document.createElement('p');
+        horaParrafo.innerHTML = `<span >Hora: </span> ${hora}`;
 
-//         const horaParrafo = document.createElement('p');
-//         horaParrafo.innerHTML = `<span class="font-weight-bolder">Hora: </span> ${hora}`;
+        const cantidadParrafo = document.createElement('p');
+        cantidadParrafo.innerHTML = `<span >Cantidad de Personas: </span> ${cantidad}`;
 
-//         const sintomasParrafo = document.createElement('p');
-//         sintomasParrafo.innerHTML = `<span class="font-weight-bolder">Síntomas: </span> ${sintomas}`;
+        //Agregar un contenedor para los botones de eliminar y editar
+        const containerButtons = document.createElement('div');
+        containerButtons.classList.add('flex', 'justify-center', 'items-center');
+        
+        //Agregar un botón de eliminar...
+        const btnEliminar = document.createElement('button');
+        //btnEliminar.onclick = () => eliminarCita(id); // añade la opción de eliminar
+        btnEliminar.classList.add('bg-red-500', 'text-white', 'p-1', 'rounded-xl', 'flex', 'justify-center', 'items-center');
+        btnEliminar.innerHTML = '<span>Eliminar</span><svg class="w-1/5 h-1/5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
 
-//         // Agregar un botón de eliminar...
-//         const btnEliminar = document.createElement('button');
-//         btnEliminar.onclick = () => eliminarCita(id); // añade la opción de eliminar
-//         btnEliminar.classList.add('btn', 'btn-danger', 'mr-2');
-//         btnEliminar.innerHTML = 'Eliminar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>'
+        // Añade un botón de editar...
+         const btnEditar = document.createElement('button');
+        // btnEditar.onclick = () => cargarEdicion(cita);
 
-//         // Añade un botón de editar...
-//         const btnEditar = document.createElement('button');
-//         btnEditar.onclick = () => cargarEdicion(cita);
+         btnEditar.classList.add('bg-green-500', 'text-white', 'p-1', 'rounded-xl', 'flex', 'justify-center', 'items-center');
+         btnEditar.innerHTML = 'Editar <svg class="w-1/5 h-1/5" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
 
-//         btnEditar.classList.add('btn', 'btn-info');
-//         btnEditar.innerHTML = 'Editar <svg fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"></path></svg>'
+         containerButtons.appendChild(btnEliminar)
+         containerButtons.appendChild(btnEditar)
 
-//         // Agregar al HTML
-//         divCita.appendChild(mascotaParrafo);
-//         divCita.appendChild(propietarioParrafo);
-//         divCita.appendChild(telefonoParrafo);
-//         divCita.appendChild(fechaParrafo);
-//         divCita.appendChild(horaParrafo);
-//         divCita.appendChild(sintomasParrafo);
-//         divCita.appendChild(btnEliminar)
-//         divCita.appendChild(btnEditar)
+        // Agregar al HTML
+        divCita.appendChild(nombreH2);
+        divCita.appendChild(telefonoParrafo);
+        divCita.appendChild(fechaParrafo);
+        divCita.appendChild(horaParrafo);
+        divCita.appendChild(cantidadParrafo);
+        divCita.appendChild(containerButtons);
+        // divCita.appendChild(btnEliminar)
+        // divCita.appendChild(btnEditar)
 
-//         contenedorCitas.appendChild(divCita);
-//     });    
-// }
+        reservasActualesContainer.appendChild(divCita);
+    });    
+}
+
+    limpiarHTML() {
+        while(reservasActualesContainer.firstChild) {
+            reservasActualesContainer.removeChild(reservasActualesContainer.firstChild);
+        }
+    }
+
 }
 
 
@@ -138,7 +151,7 @@ function datosCita(e) {
 
 
 function nuevaCita(e) {
-     e.preventDefault();
+    e.preventDefault();
     const {nombre, telefono, fecha, hora, cantidad } = citaObj;
 
     // Validar
@@ -148,17 +161,17 @@ function nuevaCita(e) {
         return;
     }
 
-// generar un id único
-citaObj.id = Date.now();
+    // generar un id único
+    citaObj.id = Date.now();
 
-// Creando una nueva cita
-administrarCitas.agregarCita({...citaObj});
+    // Creando una nueva cita
+    administrarCitas.agregarCita({...citaObj});
 
-// Reiniciar el formulario
-form.reset();
+    // Reiniciar el formulario
+    form.reset();
     
     // Imprimir el HTML de citas
-    // ui.imprimirCitas(administrarCitas);
+    ui.imprimirCitas(administrarCitas);
 
 }
 
